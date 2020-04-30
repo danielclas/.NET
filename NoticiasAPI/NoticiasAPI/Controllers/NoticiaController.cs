bffd803184fab910915e5af92cbe4a01073cfff9
Daniel Clas <danielclas@outlook.es>
2020-04-30 18:56:48 -0300

    Backup

---
+++ NoticiasAPI/NoticiasAPI/Controllers/NoticiaController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using NoticiasAPI.Services;
+using NoticiasAPI.Models;
+
+namespace NoticiasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoticiaController : ControllerBase
+    {
+        private readonly NoticiaService _noticiaService;
+        public NoticiaController(NoticiaService noticiaService)
+        {
+            _noticiaService = noticiaService;
+        }
+
+        [HttpGet]
+        [Route("obtener")]
+        public IActionResult Obtener()
+        {
+            return Ok(_noticiaService.Obtener());
+        }
+
+        [HttpPost]
+        [Route("agregar")]
+        public IActionResult Agregar([FromBody] Noticia _noticia)
+        {
+            var result = _noticiaService.Agregar(_noticia);
+            if (result)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        [Route("editar")]
+        public IActionResult Editar([FromBody] Noticia _noticia)
+        {
+            var result = _noticiaService.EditarNoticia(_noticia);
+            if (result)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult Eliminar(int id)
+        {
+            var result = _noticiaService.EliminarNoticia(id);
+            if (result)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ NoticiasAPI/NoticiasAPI/Services/NoticiaService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoticiasAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoticiasAPI.Services
+{
+    public class NoticiaService
+    {
+        private readonly NoticiaDBContext _noticiaDBContext;
+        public NoticiaService(NoticiaDBContext noticiasDBContext)
+        {
+            _noticiaDBContext = noticiasDBContext;
+        }
+
+        public bool Agregar(Noticia _noticia)
+        {
+            try
+            {
+                _noticiaDBContext.Noticia.Add(_noticia);
+                _noticiaDBContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public List<Noticia> Obtener()
+        {
+            return _noticiaDBContext.Noticia.Include(x => x.Autor).ToList();
+        }
+
+        public bool EditarNoticia(Noticia _noticia)
+        {
+            try
+            {
+                var noticiaDB = _noticiaDBContext.Noticia.Where(x => x.NoticiaID == _noticia.NoticiaID).FirstOrDefault();
+                noticiaDB.Titulo = _noticia.Titulo;
+                noticiaDB.Descripcion = _noticia.Descripcion;
+                noticiaDB.Contenido = _noticia.Contenido;
+                noticiaDB.AutorID = _noticia.AutorID;
+                noticiaDB.Fecha = _noticia.Fecha;
+                _noticiaDBContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool EliminarNoticia(int noticiaID)
+        {
+            try
+            {
+                var noticiaDB = _noticiaDBContext.Noticia.Where(x => x.NoticiaID == noticiaID).FirstOrDefault();
+                _noticiaDBContext.Noticia.Remove(noticiaDB);
+                _noticiaDBContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
